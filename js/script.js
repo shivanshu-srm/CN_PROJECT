7316a0f03a73e2212df1e4a0342ec1ad6fb64fe6
SHIVANSHU DIXIT <103733101+shivanshu-srm@users.noreply.github.com>
2022-11-13 22:05:40 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,16 +14,3 @@ document.querySelector('#search-btn').onclick = () =>{
     cartItem.classList.remove('active');
 }
 
-let cartItem = document.querySelector('.cart-items-container');
-
-document.querySelector('#cart-btn').onclick = () =>{
-    cartItem.classList.toggle('active');
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-}
-
-window.onscroll = () =>{
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-}
